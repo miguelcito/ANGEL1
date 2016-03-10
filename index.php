3e70d6684419d5440845b8ddc81372353e18b403
miguel <espinoza@12>
2016-03-09 23:22:55 -0500

    ESPINOZA

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="public/css/bootstrap.css">
+
+</head>
+<body>
+
+
+
+<div class="container">
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">FIRE FOX </span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">OFICINAS</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		
+		 <div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">ESPINOZA</a></li>
+				<li><a href="#">ALQUILER</a></li>
+			</ul>
+			<header id="header" class="">
+				
+			</header><!-- /header -->
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Buscar Aqui">
+				</div>
+				<button type="submit" class="btn btn-default">HUBROWT</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#"></a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown"> HERMANOS MIGUEL <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="">MIGUEL ESPINOZA</a></li>
+						<li><a href="#">ANGEL ESPINOZA </a></li>
+						<li><a href="#"> FACEBOOCK </a></li>
+						<li><a href="#">WHATSAP</a></li>
+						<li><a href="">955595990</a></li>
+						<li><a href="#">ESTUDIANTE</a></li>
+						<li><a href="#">AMABLE</a></li>
+						<li><a href="#">ROBA CORAZON</a></li>
+
+					</ul>
+				</li>
+			</ul>
+		 </div><!-- /.navbar-collapse -->
+		
+	</div>
+</nav>
+<!--SLIDER-->
+<center>
+<div id="carousel-id" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+		<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="item">
+			<img data-src="public/imagenes/angel.jpg" alt="First slide" src="public/imagenes/angel.jpg" width="1010">
+			<div class="container">
+				<div class="carousel-caption">
+					
+					<p><a class="btn btn-lg btn-primary" target="_blank" href="" role="button"></a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<img data-src="public/imagenes/miguel.jpg" alt="Second slide" src="public/imagenes/miguel.jpg" width="1010">
+			<div class="container">
+				<div class="carousel-caption">
+					
+					<p><a class="btn btn-lg btn-" href="#" role="button"></a></p>
+				</div>
+			</div>
+		</div>
+		<div class="item active">
+			<img data-src="public/imagenes/montes.jpg" alt="Third slide" src="public/imagenes/montes.jpg" width="1010">
+
+			<div class="container">
+				<div class="carousel-caption">
+					
+					<p><a class="btn btn-lg btn-primary" target="_blank" href="" role="button"></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+	<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+</center>
+</div>
+
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="public/js/jquery-1.12.1.js"></script>
+<script src="public/js/bootstrap.js"></script>
+	
+</body>
+</html>
